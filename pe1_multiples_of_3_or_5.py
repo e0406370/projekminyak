75effe261c04ba64ebde18aec3cd4c97517d70b9
e0406370 <e0406370@u.nus.edu>
2024-11-13 20:52:55 +0800

    chore: edit description and others for pe1

--- pe1_multiples_of_3_or_5.py
+++ pe1_multiples_of_3_or_5.py
@@ -1,9 +1,12 @@
-# Problem 1 - Multiples of 3 or 5
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+  Problem 1 - Multiples of 3 or 5
+  
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+  Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 
 # standard for loop
-def method1(num : int) :
+def method1(num: int) -> int:
   sum = 0
   
   for i in range(num) :
@@ -12,8 +15,8 @@ def method1(num : int) :
   
   return sum
 
-# generator expression
-def method2(num : int) :
+# generator comprehension
+def method2(num: int) -> int:
   return sum(x for x in range(num) if x % 3 == 0 or x % 5 == 0)
 
 print(method1(1000))
